6bafd68e6b68560a4341bf6a1b70bd6e4581a19a
Nayan17107 <prajapatinayan17107@gmail.com>
2026-02-09 11:03:44 +0530

    09/02/2026

--- PR-7 Admin-Panel/Middleware/fleshMessage.js
+++
@@ -1,9 +0,0 @@
-const fleshMessage = (req, res, next) => {
-    res.locals.success_msg = req.flash('success');
-    res.locals.error_msg = req.flash('error');
-    res.locals.warning_msg = req.flash('warning');
-    res.locals.info_msg = req.flash('info');
-    next()
-}
-
-module.exports = fleshMessage;
